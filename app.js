b07a037d88ecfcd24d8de6c8d97b09fba906b9dd
Dawid <dawidgalsino@vp.pl>
2019-09-24 09:47:04 +0200

    started working on default scraper

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 
 const getJoobleOffers = require('./3rd_party_api/jooble/index');
 
-getJoobleOffers('junior developer', 'Warszawa', (data)=> {
-    console.log(data)
-})
+// getJoobleOffers('junior developer', 'Warszawa', (data)=> {
+//     console.log(data)
+// })
+
+const scrapeSite = require('./scrapers/scraper_function/scraper_index');
+
+
+const settings =  {
+	keywords: ['junior', 'front', 'end'],
+	location: 'warszawa',
+	jobListingContainersSelector: '.results__list-container-item .offer-details__title-link',
+
+	//Site selectors, used to get information from dom individual ofer
+	offerDOMSelectors: {
+		jobTitleSelector: '#offerTitle',
+		datePostedSelector: 'span[itemprop="datePosted"]',
+		employerSelector: '#offerEmployer',
+		addressSelector: 'span[itemprop="addressRegion"]',
+	},
+
+	searchUrl: ()=> {
+		return `https://www.pracuj.pl/praca/${this.keywords.join("%20")};kw/${this.location};wp`
+	} 
+}
+
+scrapeSite(settings)
--- scrapers/scraper_function/scraper_index.js
+++ scrapers/scraper_function/scraper_index.js
@@ -17,67 +17,80 @@ const fs = require('fs');
 //Search variables
 
 const settings =  {
-	keywords: 'junior front end',
+	keywords: ['junior', 'front', 'end'],
 	location: 'warszawa',
-	siteUrl: `https://www.pracuj.pl/praca/junior%20front%20end;kw/${this.location};wp`
+	jobListingContainersSelector: '.results__list-container-item .offer-details__title-link',
+
+	//Site selectors, used to get information from dom individual ofer
+	offerDOMSelectors: {
+		jobTitleSelector: '#offerTitle',
+		datePostedSelector: 'span[itemprop="datePosted"]',
+		employerSelector: '#offerEmployer',
+		addressSelector: 'span[itemprop="addressRegion"]',
+	},
+
+	searchUrl: function() {
+		//need to understand more on why this is undefined
+		return `https://www.pracuj.pl/praca/${this.keywords.join("%20")};kw/${this.location};wp`
+	} 
 }
 
 const scrapeSite = (settings) => {
-	console.log('test')
+	
+	console.log(settings.searchUrl())
+
+	// requestPromise(settings.searchUrl)
+	// 	.then(function(html){
+	// 		//success!
+	// 		//console.log(html);
+			
+	// 		//3 Parsing HTML with Cheerio.js
+	// 		//console.log($(containersSelector, html));
+	// 		//console.log($('big > a', html));
+	// 		const offerUrls = [];
+	// 		const offersNumber =$(settings.jobListingContainersSelector, html).length
+
+			
+	// 		for (let i = 0; i < offersNumber; i++){
+	// 			//4 filter only the links
+	// 			offerUrls.push($(settings.jobListingContainersSelector, html)[i].attribs.href);
+	// 		}
+			
+			
+			
+	// 		return Promise.all(
+	// 			offerUrls.map(function(offerUrl){
+	// 				//console.log(siteUrl + offerUrl)
+	// 				return offerParse(searchUrl + offerUrl)
+	// 			})
+	// 		)
+
+			
+	// 	})
+		
+	// 	.then(function(jobOffers){
+	// 		const jobOffersString = JSON.stringify(jobOffers)
+	// 		//console.log(jobOffersString)
+			
+	// 		fs.writeFile("./jsonoutput/jobs.json", jobOffersString, function(err){
+	// 			if(err){
+	// 				return console.log(err)
+	// 			}
+				
+	// 			console.log("###File was saved!###")
+	// 		})
+	// 	})
+			
+		
+	// 	.catch(function(err){
+	// 		//handle error
+	// 		console.log(err);
+	// 	});
+
 }
 
- const url = 'https://www.pracuj.pl/praca/junior%20front%20end;kw/warszawa;wp';
- const containersSelector = '.results__list-container-item .offer-details__title-link';
 
 
-
-
-
-rp(url)
-  .then(function(html){
-    //success!
-    //console.log(html);
-	
-	//3 Parsing HTML with Cheerio.js
-	 //console.log($(containersSelector, html));
-     //console.log($('big > a', html));
-	const offerUrls = [];
-	const offersNumber =$(containersSelector, html).length
-
-	
-	for (let i = 0; i < offersNumber; i++){
-		//4 filter only the links
-		offerUrls.push($(containersSelector, html)[i].attribs.href);
-	}
-	
-	
-	
-	return Promise.all(
-		offerUrls.map(function(offerUrl){
-			//console.log(siteUrl + offerUrl)
-			return offerParse(siteUrl + offerUrl)
-		})
-	)
-
-	
-  })
   
-  .then(function(jobOffers){
-	  const jobOffersString = JSON.stringify(jobOffers)
-     //console.log(jobOffersString)
-	 
-	 fs.writeFile("./jsonoutput/jobs.json", jobOffersString, function(err){
-		 if(err){
-			 return console.log(err)
-		 }
-		 
-		 console.log("###File was saved!###")
-	 })
-   })
-	
-  
-  .catch(function(err){
-    //handle error
-	console.log(err);
-  });
-  
+
+  module.exports = scrapeSite;
